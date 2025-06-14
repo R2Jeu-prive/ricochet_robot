5585f97b10ddf3c41f812a7ce543ee3594fecb74
Pierre <R2Jeu.prive@gmail.com>
2025-06-14 21:37:14 +0200

    working passing walls

--- rust_solver/src/lib.rs
+++ rust_solver/src/lib.rs
@@ -1,11 +1,21 @@
-use queues::*;
+use std::collections::HashMap;
+
+//use queues::*;
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
 
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
 }
 
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+}
+
+/*
 #[derive(Clone, Copy)]
 enum Move {
     VoidUp,
@@ -29,47 +39,20 @@ enum Move {
     YellowLeft,
     YellowRight,
 }
+*/
 
-#[derive(Clone)]
-struct GameState {
-    void: (u8, u8),
-    red: (u8, u8),
-    green: (u8, u8),
-    blue: (u8, u8),
-    yellow: (u8, u8),
-    move_count: u8,
-    moves: [Move; 25]
+#[derive(Serialize, Deserialize)]
+pub struct Board {
+    pub bob: String,
+    pub walls: Vec<HashMap<String, u8>>,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-fn js_to_rust(js : JsValue) -> GameState{
-    return GameState { void: (1, 1), red: (0, 0), green: (15, 0), blue: (4, 15), yellow: (3, 9), move_count: 0, moves: [Move::VoidUp; 25]};
-}
-
-fn rust_to_js(state : GameState) -> JsValue{
-    return JsValue::from_f64(state.move_count.into());
-}
-
-
-#[wasm_bindgen]
-pub fn solve(start_state : JsValue, depth : u8) -> JsValue{
-    let states_max_count: usize = 100000;
-    let mut states: Buffer<GameState> = Buffer::new(states_max_count);
-
-    let res = states.add(js_to_rust(start_state));
-
-    loop {
-        let state : GameState = states.remove().expect("Queue should not be empty");
-
-        
-
-        if states.size() == 0 {
-            return None;
-        }
-    }
+pub fn set_board(board : JsValue) -> Result<(), JsValue>{
+    let b: Board = serde_wasm_bindgen::from_value(board)?;
+    log("Hello World from Rust !");
+    log(b.bob.as_str());
+    log(b.walls.len().to_string().as_str());
+    Ok(())
 }
 
